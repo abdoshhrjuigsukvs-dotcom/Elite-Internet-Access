d87418f618d8cc9e0f04f3bbec4dbe22f1a457b7
abdoshhrjuigsukvs-dotcom <abdoshhrjuigsukvs@gmail.com>
2026-05-16 20:11:09 +0000

    add missing MainActivity

---
+++ app/src/main/java/com/elite/loot/MainActivity.java
@@ -0,0 +1,11 @@
+package com.elite.loot;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // تم إنشاء التطبيق بنجاح
+    }
+}
